db34252ffc001a6620e8b011be44fb4a38fdba2a
GoGstickGo <4464170+GoGstickGo@users.noreply.github.com>
2021-06-12 07:19:58 +0100

    Fix test (#5)
    * feat: add codeowners
    
    * fix: test
    
    * fix: .gitignore
    
    * chore: rm file

--- rapidapis/rapidapis_test.go
+++ rapidapis/rapidapis_test.go
@@ -1,7 +1,6 @@
 package rapidapis
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 )
@@ -58,8 +57,8 @@ func TestDarkSkyClient_convertMap(t *testing.T) {
 		want    []string
 		wantErr bool
 	}{
-		{"good", fields{data: testMapInterface}, testSlice, false},
-		{"emptySlice", fields{data: testMapInterfaceMissingDaily}, nil, true},
+		{"good", fields{mapping: Mapping{recieved: testMapInterface}}, testSlice, false},
+		{"emptySlice", fields{mapping: Mapping{recieved: testMapInterfaceMissingDaily}}, nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -71,7 +70,6 @@ func TestDarkSkyClient_convertMap(t *testing.T) {
 				countryCode: tt.fields.countryCode,
 			}
 			got, err := c.convertMap()
-			fmt.Println(got)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("DarkSkyClient.convertMap() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -130,21 +128,55 @@ func TestDarkSkyClient_getTempH(t *testing.T) {
 	}
 }
 
-func TestGeoDB_getCityLocation(t *testing.T) {
+func TestGeoDBClient_getCountryCode(t *testing.T) {
+	testSliceCity1 := []string{"[map[city:Budapest", "country:Hungary", "countryCode:HU", "id:51643", "latitude:47.498333333", "longitude:19.040833333", "name:Budapest", "population:1.752286e+06", "region:Budapest", "regionCode:BU type:CITY wikiDataId:Q1781]]"}
+	testSliceCity2 := []string{"[map[city:Budapest", "country:Hungary", "countryCode", "id:51643", "latitude:47.498333333", "longitude:19.040833333", "name:Budapest", "population:1.752286e+06", "region:Budapest", "regionCode:BU type:CITY wikiDataId:Q1781]]"}
+
+	type fields struct {
+		data    map[string]interface{}
+		err     error
+		mapping Mapping
+		params  Params
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+		{"good", fields{mapping: Mapping{tempField: testSliceCity1}}, "HU", false},
+		{"error", fields{mapping: Mapping{tempField: testSliceCity2}}, "", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &GeoDBClient{
+				data:    tt.fields.data,
+				err:     tt.fields.err,
+				mapping: tt.fields.mapping,
+				params:  tt.fields.params,
+			}
+			got, err := c.getCountryCode()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GeoDBClient.getCountryCode() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GeoDBClient.getCountryCode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGeoDBClient_getCityLocation(t *testing.T) {
 	testSliceCity1 := []string{"[map[city:Budapest", "country:Hungary", "countryCode:HU", "id:51643", "latitude:47.498333333", "longitude:19.040833333", "name:Budapest", "population:1.752286e+06", "region:Budapest", "regionCode:BU type:CITY wikiDataId:Q1781]]"}
 	testSliceCity2 := []string{"[map[city:Budapest", "country:Hungary", "countryCode:HU", "id:51643", "latitude:", "longitude:19.040833333", "name:Budapest", "population:1.752286e+06", "region:Budapest", "regionCode:BU type:CITY wikiDataId:Q1781]]"}
 	testSliceCity3 := []string{"[map[city:Budapest", "country:Hungary", "countryCode:HU", "id:51643", "name:Budapest", "population:1.752286e+06", "region:Budapest", "regionCode:BU type:CITY wikiDataId:Q1781]]"}
 
 	type fields struct {
-		recieved    map[string]interface{}
-		err         error
-		tempField   []string
-		tempSplit   []string
-		latitude    string
-		longitude   string
-		countryCode string
-		pass        map[string]string
-		value       string
+		data    map[string]interface{}
+		err     error
+		mapping Mapping
+		params  Params
 	}
 	tests := []struct {
 		name    string
@@ -153,82 +185,28 @@ func TestGeoDB_getCityLocation(t *testing.T) {
 		want1   string
 		wantErr bool
 	}{
-		{"good", fields{tempField: testSliceCity1}, "47.498333333", "19.040833333", false},
-		{"missingLatitude", fields{tempField: testSliceCity2}, "", "19.040833333", true},
-		{"missingLong&Lat", fields{tempField: testSliceCity3}, "", "", true},
+		{"good", fields{mapping: Mapping{tempField: testSliceCity1}}, "47.498333333", "19.040833333", false},
+		{"missingLatitude", fields{mapping: Mapping{tempField: testSliceCity2}}, "", "19.040833333", true},
+		{"missingLong&Lat", fields{mapping: Mapping{tempField: testSliceCity3}}, "", "", true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			m := &GeoDB{
-				recieved:    tt.fields.recieved,
-				err:         tt.fields.err,
-				tempField:   tt.fields.tempField,
-				tempSplit:   tt.fields.tempSplit,
-				latitude:    tt.fields.latitude,
-				longitude:   tt.fields.longitude,
-				countryCode: tt.fields.countryCode,
-				pass:        tt.fields.pass,
-				value:       tt.fields.value,
+			c := &GeoDBClient{
+				data:    tt.fields.data,
+				err:     tt.fields.err,
+				mapping: tt.fields.mapping,
+				params:  tt.fields.params,
 			}
-			got, got1, err := m.getCityLocation()
+			got, got1, err := c.getCityLocation()
 			if (err != nil) != tt.wantErr {
-				t.Errorf("GeoDB.getCityLocation() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("GeoDBClient.getCityLocation() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got != tt.want {
-				t.Errorf("GeoDB.getCityLocation() got = %v, want %v", got, tt.want)
+				t.Errorf("GeoDBClient.getCityLocation() got = %v, want %v", got, tt.want)
 			}
 			if got1 != tt.want1 {
-				t.Errorf("GeoDB.getCityLocation() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
-}
-
-func TestGeoDB_getCountryCode(t *testing.T) {
-	testSliceCity1 := []string{"[map[city:Budapest", "country:Hungary", "countryCode:HU", "id:51643", "latitude:47.498333333", "longitude:19.040833333", "name:Budapest", "population:1.752286e+06", "region:Budapest", "regionCode:BU type:CITY wikiDataId:Q1781]]"}
-	testSliceCity2 := []string{"[map[city:Budapest", "country:Hungary", "countryCode", "id:51643", "latitude:47.498333333", "longitude:19.040833333", "name:Budapest", "population:1.752286e+06", "region:Budapest", "regionCode:BU type:CITY wikiDataId:Q1781]]"}
-
-	type fields struct {
-		recieved    map[string]interface{}
-		err         error
-		tempField   []string
-		tempSplit   []string
-		latitude    string
-		longitude   string
-		countryCode string
-		pass        map[string]string
-		value       string
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		want    string
-		wantErr bool
-	}{
-		{"good", fields{tempField: testSliceCity1}, "HU", false},
-		{"error", fields{tempField: testSliceCity2}, "", true},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			m := &GeoDB{
-				recieved:    tt.fields.recieved,
-				err:         tt.fields.err,
-				tempField:   tt.fields.tempField,
-				tempSplit:   tt.fields.tempSplit,
-				latitude:    tt.fields.latitude,
-				longitude:   tt.fields.longitude,
-				countryCode: tt.fields.countryCode,
-				pass:        tt.fields.pass,
-				value:       tt.fields.value,
-			}
-			got, err := m.getCountryCode()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GeoDB.getCountryCode() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("GeoDB.getCountryCode() = %v, want %v", got, tt.want)
+				t.Errorf("GeoDBClient.getCityLocation() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
