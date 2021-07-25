bfb1375d4891c11895380897f03cec78714a495a
GoGstickGo <4464170+GoGstickGo@users.noreply.github.com>
2021-07-25 21:18:34 +0100

    feat: web (#9)
    - simple webserver
    - gorilla mux routing
    - home/contact page
    - go html templating
    - bootstrap layout
    - add controllers (MVC pattern)
    - add REST
    - DRY
    - use GORM for interact with DB
    - add city controllers/models
    - update routing
    - save return values in db
    - remove user related
    - different error handlening for web
    - connect BE to FE
    - add alerting in UI
    - NonApi feature

---
+++ web/controllers/city.go
@@ -0,0 +1,106 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"weather-api/rapidapis"
+	"weather-api/utils"
+	"weather-api/web/models"
+	"weather-api/web/views"
+)
+
+type CityRequest struct {
+	CityView       *views.View
+	CityReturnView *views.View
+	c              *models.CityService
+}
+
+type CityForm struct {
+	Name  string `schema:"name"`
+	Day   string `schema:"day"`
+	Month string `schema:"month"`
+	Year  string `schema:"year"`
+}
+
+func NewRequest(c *models.CityService) *CityRequest {
+	return &CityRequest{
+		CityView:       views.NewView("bootstrap", "wtm/city"),
+		CityReturnView: views.NewView("bootstrap", "wtm/cityreturn"),
+		c:              c,
+	}
+}
+
+func (c *CityRequest) New(w http.ResponseWriter, r *http.Request) {
+	if err := c.CityView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func (c *CityRequest) GetTemps(w http.ResponseWriter, r *http.Request) {
+	var vd views.Data
+	var form CityForm
+
+	if err := parseForm(r, &form); err != nil {
+		log.Println(err)
+		vd.Alert = &views.Alert{
+			Level:   views.AlertLvlError,
+			Message: views.AlertGenericMsg,
+		}
+	}
+	date, err := utils.BuildDate(form.Year, form.Month, form.Day)
+	if err != nil {
+		log.Println(err)
+		vd.Alert = &views.Alert{
+			Level:   views.AlertLvlError,
+			Message: fmt.Sprintf("%v", err),
+		}
+	}
+	freeCities, _ := c.c.FreeCity(date, form.Name)
+	if err != nil {
+		log.Println(err)
+		vd.Alert = &views.Alert{
+			Level:   views.AlertLvlError,
+			Message: fmt.Sprintf("%v", err),
+		}
+	}
+	if freeCities != nil {
+		log.Println("free", freeCities)
+		c.CityReturnView.Render(w, freeCities)
+	} else {
+		tempH, tempL, date, city, cc, err := rapidapis.DsReturnsWeb(rapidapis.Params{
+			City:   form.Name,
+			Day:    form.Day,
+			Month:  form.Month,
+			Year:   form.Year,
+			Apikey: os.Getenv("RAPIDAPI_KEY"),
+		})
+		if err != nil {
+			log.Println(err)
+			vd.Alert = &views.Alert{
+				Level:   views.AlertLvlError,
+				Message: fmt.Sprintf("%v", err),
+			}
+			c.CityView.Render(w, vd)
+			return
+		}
+
+		cities := models.Cities{
+			City:        city,
+			Date:        date,
+			CountryCode: cc,
+			TempHigh:    tempH,
+			TempLow:     tempL,
+		}
+
+		if err := c.c.Create(&cities); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		log.Print("not free")
+		c.CityReturnView.Render(w, cities)
+
+	}
+
+}
